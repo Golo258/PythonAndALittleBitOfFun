2792cb6bce2a2aba3fb239dc3bd2ad63adfe7feb
Grzegorz Golonka <golon338@gmail.com>
2025-10-18 16:18:23 +0100

    a little bit of playing with subprocesses

---
+++ Notes/Knowleadge.py
@@ -0,0 +1,76 @@
+import os
+import subprocess
+
+from argparse import ArgumentParser
+from time import sleep
+
+class Log():
+    @classmethod
+    def debug(self, message): # only for now, latter i will build better with colors
+        print(f"[DEBUG] {message}")
+
+class GitSetup(object):
+
+    def run_git_cmd(self):
+        subprocess.call("git config --global --list")
+        subprocess.call("git ")
+
+    def setup_user(self):
+        pass
+
+
+class Utils(object):
+
+    def get_cmd_arguments(self, dict_args):
+        parser = ArgumentParser()
+        # TODO implement this badboy with parsing dict of arguments
+
+
+class SubprocessPlayground(object):
+    """
+        Running shell scripts / commands with usage of subprocesses
+        run | call | popen differ in how execute command and
+            how they manage output and return codes
+
+        Multiprocessing is for parallel execution of processes
+        subprocess is manage external processes like ping ls make and more
+        to run multiple command using subprocess we can use pipes
+
+        subprocess - module to running child-processes - means external programs
+        usually we use subprocess.run and Popen - to have more control
+            call is deprecated and unused so we are not going to learn this badboy
+    """
+
+    def simple_run(self, ):
+        result = subprocess.run([
+            "wsl",   # wsl because iam on windows now
+            "echo",
+            "What's going on in here"
+        ])
+        Log.debug(f"Result: {result.returncode}")
+
+    def run_with_capture(self):
+        output = subprocess.run([
+            "wsl",
+            "echo",
+            "Am I going to be captured?"
+        ],
+            capture_output=True,
+            text=True
+        )
+        if output.returncode == 0 :
+            Log.debug(f"Captured text: {output.stdout} // Yes You do.")
+        else:
+            Log.debug(f"Captured errors: {output.stderr} // There is a problem in here.")
+
+    def simple_popen(self):
+        pass
+
+
+setup = GitSetup()
+# setup.run_git_cmd()
+
+
+sub_playground = SubprocessPlayground()
+sub_playground.simple_run()
+sub_playground.run_with_capture()
